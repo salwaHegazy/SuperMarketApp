5dbe23e7a62f39c6d3d43af870a6a49f029baa43
Salwa Hegazy <salwahegazy@192.168.117.128>
2018-10-03 22:28:11 +0200

    first commit

--- D2B SuperMarket/ViewController.swift
+++ D2B SuperMarket/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var fastFoodItems = ["KFC" , "Koshary" , "Kabab"]
+    var fruitItems = ["Orange" , "Mango" , "Banana" ]
+    var sectionsTypes = ["Fast Food" , "Fruits"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+ func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionsTypes.count
+    }
+    
+    
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            
+            return fastFoodItems.count
+            
+        }else {
+            
+            return fruitItems.count
+        }
+        
+    }
+    
+    
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell" , for:indexPath)
+        
+        
+        if indexPath.section == 0 {
+            
+            
+            cell.textLabel?.text=fastFoodItems[indexPath.row]
+            
+        }else {
+            
+            cell.textLabel?.text=fruitItems[indexPath.row]
+            
+            
+        }
+        
+        
+        return cell
+    }
+    
+    
+func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return sectionsTypes[0]
+            
+        }else{
+            
+            return sectionsTypes[1]
+        }
+        
+    }
+    
+    
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-
+    let cell = tableView.cellForRow(at: indexPath)
+    
+    if indexPath.section == 0{
+        
+        print(fastFoodItems[indexPath.row])
+    
+    }else {
+    
+        print(fruitItems[indexPath.row])
+    }
+    
+    }
+    
+    
 }
 
